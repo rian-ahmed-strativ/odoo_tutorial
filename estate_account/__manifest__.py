08a1234757de42859fac671675f00b87d874b382
Rian Ahmed <rian@Rians-MacBook-Air.local>
2025-07-08 13:20:58 +0600

    Feat: added invoice feature

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Estate Account',
+    'version': '1.0',
+    'summary': 'Link module between Real Estate and Accounting',
+    'description': 'This module integrates Estate with the Accounting app.',
+    'depends': ['estate', 'account'],
+    'data': [
+        'views/estate_property_views.xml',
+             ],
+    'application': False,
+    'installable': True,
+}
