911f32a3643be94a54838795062ee3fb8ca11054
Rian Ahmed <rian@Rians-MacBook-Air.local>
2025-07-08 14:04:32 +0600

    Feat: added Kanban board in Properties List.

--- real_estate/__manifest__.py
+++ real_estate/__manifest__.py
@@ -15,6 +15,7 @@
         'views/estate_menus.xml',
         'views/estate_property_views.xml',
         'views/estate_property_search.xml',
+        'views/estate_property_kanban_views.xml',
         'views/estate_property_tag_views.xml',
         'views/res_users_views.xml',
     ],
